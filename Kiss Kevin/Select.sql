83a15ed1f4b334ac3382f65dc3b8b24a7ebc8d15
Kiss Kevin <kevin.kiss@katonaj-mkszig.hu>
2022-09-21 12:29:25 +0200

    select gykorlat

---
+++ Kiss Kevin/Select.sql
@@ -0,0 +1,16 @@
+1. Feladat
+a, SELECT * FROM Customers
+b, SELECT CategoryName FROM Customers
+c, SELECT CategoryName, Description FROM Customers
+
+
+2. Feladat
+a, SELECT ProductName, Unit, Price FROM Products
+b, SELECT ProductName, Price FROM Products WHERE price < 20
+c, SELECT ProductName, Price FROM Products WHERE price BETWEEN 20 AND 30
+d, SELECT ProductName, Price FROM Products WHERE price <> 10
+e, SELECT ProductName FROM Products WHERE ProductName LIKE 'T%'
+f, SELECT ProductName FROM Products WHERE ProductName LIKE 'Louisiana%'
+
+3. Feladat
+SELECT DISTINCT Country FROM Suppliers
