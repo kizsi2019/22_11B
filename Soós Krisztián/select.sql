f266e69bada5be18dcdec9d4bd5148cf64aa553b
Soós Krisztián <90073650+SoosKrisztian@users.noreply.github.com>
2022-09-21 12:23:50 +0200

    Add files via upload

---
+++ Soós Krisztián/select.sql
@@ -0,0 +1,20 @@
+1, feladat
+a) SELECT * FROM [Categories]
+b) SELECT CategoryName FROM [Categories]
+c) SELECT CategoryName, Description FROM [Categories]
+
+2,
+a)SELECT ProductName, Unit, Price FROM [Products]
+b)SELECT ProductName, Price FROM [Products]
+WHERE price < 20;
+c)SELECT ProductName, Price FROM [Products]
+WHERE price BETWEEN 20 AND 30;
+d)SELECT ProductName, Price FROM [Products]
+WHERE price <> 10;
+e)SELECT * FROM [Products]
+WHERE ProductName LIKE "T%';
+f)SELECT * FROM [Products]
+WHERE ProductName LIKE "Louisiana%';
+
+3,
+a)SELECT DISTINCT Country FROM [Suppliers]
